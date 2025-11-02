3a56eb5e71aa58067fe0f8e43fda1cab2ad07318
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2025-11-03 00:35:24 +0700

    feat: add submit question and simple frontend

--- lib/realtime_qa_web/router.ex
+++ lib/realtime_qa_web/router.ex
@@ -18,6 +18,8 @@ defmodule RealtimeQaWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live("/question", Question)
   end
 
   # Other scopes may use custom stacks.
